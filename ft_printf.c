9df86cda494bbcf6a7219037880af8e555f91e6f
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-27 11:15:39 +0200

    added libft

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,25 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+// take the argument from '%'
+va_list	params(va_list ap) 
 {
-	va_list
+	if ap ==
+
+}
+
+// loop through the string until '%'
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	va_list	ap;
+
+	i = 0;
+	va_start(ap, str);
+
+	while (str[i - 1] != '%' && str[i])
+		ft_putchar(i++)
+
+	va_end(ap);
+
+
 }
