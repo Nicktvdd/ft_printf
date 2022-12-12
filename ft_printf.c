fea52c2ea64ce4e43dade368aae29cc66040d04a
Nick van den Dungen <nicktvdd@hotmail.com>
2022-12-12 11:23:18 +0200

    Small fixes, compiles now

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:34:30 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/12/12 11:17:43 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/12/12 11:23:08 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,29 @@ int	ft_putnbr(int n)
 }
 
 // take the argument from '%'
-int	params(char *str, va_list ap) 
+int	params(const char *str, va_list ap) 
 {
-	if (str == 'c')
-		ft_putchar(str);//Prints a single character.
+	if (*str == 'c')
+		ft_putchar(*str);//Prints a single character.
 /* 	if (ap == 's')
 		ft_putstr(str);// Prints a string (as defined by the common C convention).
 	if (ap == 'p')
 		ft_void(str);// The void * pointer argument has to be printed in hexadecimal format. */
-	if (str == 'd' || ap == 'i')
-		ft_putnbr(str);// Prints a decimal (base 10) number.
-	if (str == 'u')
-		ft_putnbr(str);// Prints an unsigned decimal (base 10) number.
+	if (*str == 'd' || *str == 'i')
+		ft_putnbr(*str);// Prints a decimal (base 10) number.
+	if (*str == 'u')
+		ft_putnbr(*str);// Prints an unsigned decimal (base 10) number.
 /* 	if (ap == 'x')
 		ft_lowhex(str);// Prints a number in hexadecimal (base 16) lowercase format.
 	if (ap == 'X')
 		ft_uphex(str);// Prints a number in hexadecimal (base 16) uppercase format. */
-	if (str == '%')
-		write(1, '%', 1);// Prints a percent sign.
+	if (*str == '%')
+		write(1, "%", 1);// Prints a percent sign.
 	return (0);
 }
 
 // loop through the string until '%'
+
 int	ft_printf(const char *str, ...)
 {
 	int			i;
