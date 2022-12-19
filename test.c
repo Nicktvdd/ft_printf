71e2c159ca07666145a53a750d105fe466ae5bcf
Nick van den Dungen <nicktvdd@hotmail.com>
2022-12-19 16:07:54 +0200

    tests for value

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:35:59 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/12/19 15:36:05 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/12/19 16:07:00 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,50 @@ int	main(void)
 
 	value = 0;
 
-	ft_printf("c is :%c:\n", 42);
-	printf("c is :%c:\n", 42);
+	value = ft_printf("c is :%c:\n", 42);
+	printf("value is %i\n", value);
+	value = printf("c is :%c:\n", 42);
+	printf("value is %i\n\n", value);
 
-	ft_printf("i is :%i:\n", 1);
-	printf("i is :%i:\n", 1);
+	value = ft_printf("i is :%i:\n", 1);
+	printf("value is %i\n", value);
+	value = printf("i is :%i:\n", 1);
+	printf("value is %i\n\n", value);
 	
-	ft_printf("d is :%i:\n", 1);
-	printf("d is :%i:\n", 1);
+	value = ft_printf("d is :%i:\n", 1);
+	printf("value is %i\n", value);
+	value = printf("d is :%i:\n", 1);
+	printf("value is %i\n\n", value);
 
-	ft_printf("%% is :%%:\n");
-	printf("%% is :%%:\n");
+	value = ft_printf("%% is :%%:\n");
+	printf("value is %i\n", value);
+	value = printf("%% is :%%:\n");
+	printf("value is %i\n\n", value);
 	
-	ft_printf("u is :%u:\n", -1);
-	printf("u is :%u:\n", -1);
+	value = ft_printf("u is :%u:\n", -1);
+	printf("value is %i\n", value);
+	value = printf("u is :%u:\n", -1);
+	printf("value is %i\n\n", value);
 
-	ft_printf("str is :%s:\n", "stringboi");
-	printf("str is :%s:\n", "stringboi");
+	value = ft_printf("str is :%s:\n", "stringboi");
+	printf("value is %i\n", value);
+	value = printf("str is :%s:\n", "stringboi");
+	printf("value is %i\n\n", value);
 
-	ft_printf("x is :%x:\n", 10);
-	printf("x is :%x:\n", 10);
+	value = ft_printf("x is :%x:\n", 10);
+	printf("value is %i\n", value);
+	value = printf("x is :%x:\n", 10);
+	printf("value is %i\n\n", value);
 
-	ft_printf("X is :%X:\n", 10);
-	printf("X is :%X:\n", 10);
+	value = ft_printf("X is :%X:\n", 10);
+	printf("value is %i\n", value);
+	value = printf("X is :%X:\n", 10);
+	printf("value is %i\n\n", value);
 
-	ft_printf("p is :%p:\n", 10);
-	printf("p is :%p:\n", 10);
+	value = ft_printf("p is :%p:\n", 10);
+	printf("value is %i\n", value);
+	value = printf("p is :%p:\n", 10);
+	printf("value is %i\n\n", value);
 	//ft_printf("string :%c: :%s: :%p: :%d: :%i: mid-string :%u: :%x: :%x: :%X: :%%: end-string\n");
 	//printf("string :%c: :%s: :%p: :%d: :%i: mid-string :%u: :%x: :%x: :%X: :%%: end-string\n");
 
